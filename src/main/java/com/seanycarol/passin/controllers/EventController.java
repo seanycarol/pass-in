13bf3b9eec91025016fd26e5810b3b3f071871f7
seanycarol <seany@ice.ufjf.br>
2024-04-02 11:28:56 -0300

    Create endpoints

---
+++ src/main/java/com/seanycarol/passin/controllers/EventController.java
@@ -0,0 +1,39 @@
+package com.seanycarol.passin.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.seanycarol.passin.dto.event.EventIdDTO;
+import com.seanycarol.passin.dto.event.EventRequestDTO;
+import com.seanycarol.passin.dto.event.EventResponseDTO;
+import com.seanycarol.passin.services.EventService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/events")
+@RequiredArgsConstructor
+public class EventController {
+    private final EventService eventService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EventResponseDTO> getEvent(@PathVariable String id) {
+        EventResponseDTO event = this.eventService.getEventDetail(id);
+        return ResponseEntity.ok(event);
+    }   
+    
+    @PostMapping
+    public ResponseEntity<EventIdDTO> createEvent(@RequestBody EventRequestDTO body, UriComponentsBuilder uriComponentsBuilder) {
+        EventIdDTO eventIdDTO = this.eventService.createEvent(body);
+
+        var uri = uriComponentsBuilder.path("/events/{id}").buildAndExpand(eventIdDTO.eventId()).toUri();
+
+        return ResponseEntity.created(uri).body(eventIdDTO);
+    }
+}
--- src/main/java/com/seanycarol/passin/dto/event/EventResponseDTO.java
+++ src/main/java/com/seanycarol/passin/dto/event/EventResponseDTO.java
@@ -2,6 +2,9 @@ package com.seanycarol.passin.dto.event;
 
 import com.seanycarol.passin.domain.event.Event;
 
+import lombok.Getter;
+
+@Getter
 public class EventResponseDTO {
     private EventDetailDTO event;
 
